7e599e22a313f267fc0c14226f399cb82ba01c77
MujiRahayuWidodo <87989295+MujiRahayuWidodo@users.noreply.github.com>
2024-06-28 11:25:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,26 +45,19 @@ import pandas as pd
 # from st_aggrid import AgGrid
 
 def main() : 
-#   st.write('Minimal Example')
-#   st.header('This is Header')
-#   st.subheader('This is SubHeader')
-#   st.markdown('# Rendering Markdown ')
-#   st.write('Some Phytagorean Equation : ')
-#   st.latex('c^2 = a^2+b^2')
+    st.write('Minimal Example')
+    
+    st.header('This is Header')
+    st.subheader('This is SubHeader')
+    st.markdown('# Rendering Markdown ')
+    st.write('Some Phytagorean Equation : ')
+    st.latex('c^2 = a^2+b^2')
+    
     df = pd.read_csv('house_clean.csv')
     st.write('Contoh dataframe')
     st.dataframe(df)
-    # st.write('Contoh JSON')
-    # st.json(flight_passanger_api)
-    # st.write('Metrics')
-    # st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-    # st.write('Menampilkan Dataframe dengan St AgGrid')
-    # AgGrid(titanic)
-    # st.table([x for x in range(1, 5)])
-    # st.data_editor(df)
-  # st.write('Metrics')
+    
     st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-    st.write('Menampilkan Dataframe dengan St AgGrid')
 
 if __name__ == '__main__' : 
   main()
