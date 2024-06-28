3f70b352c1a1ac4636118194a24b64db1a2e4597
MujiRahayuWidodo <87989295+MujiRahayuWidodo@users.noreply.github.com>
2024-06-28 11:16:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,24 +40,35 @@
 #     ))
 
 import streamlit as st 
+import pandas as pd
+import requests
+from st_aggrid import AgGrid
 
-# def main() : 
-# #   st.write('Minimal Example')
+def main() : 
+#   st.write('Minimal Example')
 #   st.header('This is Header')
 #   st.subheader('This is SubHeader')
 #   st.markdown('# Rendering Markdown ')
 #   st.write('Some Phytagorean Equation : ')
 #   st.latex('c^2 = a^2+b^2')
-
-# if __name__ == '__main__' : 
-#   main()
-
-
-
-import pandas as pd
-
-df = pd.read_csv('house_clean.csv')
+  df = pd.read_csv('house_clean.csv')
+  st.write('Contoh dataframe')
+  st.dataframe(df)
+  st.write('Contoh JSON')
+  # st.json(flight_passanger_api)
+  # st.write('Metrics')
+  # st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+  st.write('Menampilkan Dataframe dengan St AgGrid')
+  # AgGrid(titanic)
+  st.table([x for x in range(1,5)])
 st.data_editor(df)
+
+if __name__ == '__main__' : 
+  main()
+
+
+
+
 
 
 
