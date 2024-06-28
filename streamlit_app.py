399dc6cb268bfc8f19ada89bd52b17b12ed36592
MujiRahayuWidodo <87989295+MujiRahayuWidodo@users.noreply.github.com>
2024-06-28 11:14:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,8 +59,7 @@ import pandas as pd
 df = pd.read_csv('house_clean.csv')
 st.data_editor(df)
 
-favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
-st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+
 
 
 
