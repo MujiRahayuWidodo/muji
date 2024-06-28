70929a1d4dee77b4e4b858b92067d19f130eb747
MujiRahayuWidodo <87989295+MujiRahayuWidodo@users.noreply.github.com>
2024-06-28 11:11:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,15 +39,23 @@
 #         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
 #     ))
 
-import streamlit as st 
+# import streamlit as st 
 
-def main() : 
-#   st.write('Minimal Example')
-  st.header('This is Header')
-  st.subheader('This is SubHeader')
-  st.markdown('# Rendering Markdown ')
-  st.write('Some Phytagorean Equation : ')
-  st.latex('c^2 = a^2+b^2')
+# def main() : 
+# #   st.write('Minimal Example')
+#   st.header('This is Header')
+#   st.subheader('This is SubHeader')
+#   st.markdown('# Rendering Markdown ')
+#   st.write('Some Phytagorean Equation : ')
+#   st.latex('c^2 = a^2+b^2')
 
-if __name__ == '__main__' : 
-  main()
+# if __name__ == '__main__' : 
+#   main()
+
+
+
+import pandas as pd
+
+df = pd.read_csv('house_clean.csv')
+df.head()
+
